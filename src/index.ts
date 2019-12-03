f0bf2cca155eea047efc2906c80d32e947651297
Joe Reeve <joseph.reeve@googlemail.com>
2019-12-03 09:51:04 +0000

    Plan It data

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ interface RSPBLayer {
     layer?: L.Layer,
     type: 'geoJSON',
     style?: any,
+    process?: (data: any) => any,
 }
 
 const rspbLayers: RSPBLayer[] = [
@@ -36,6 +37,12 @@ const rspbLayers: RSPBLayer[] = [
         type: 'geoJSON',
         style: { color: 'pink' },
     },
+    {
+        displayName: 'Planning Applications',
+        url: `/public/planit-sample.geojson`,
+        // url: `https://www.planit.org.uk/api/applics/geojson?pg_sz=50&compress&limit=50&end_date=${(new Date()).toISOString()}`,
+        type: 'geoJSON',
+    },
 ];
 
 const map = L.map('map').setView([51.505, -0.09], 13);
@@ -46,22 +53,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// const shpfile = new (L as any).Shapefile('/public/IBAs.zip', {
-//     onEachFeature: function (feature: any, layer: any) {
-//         if (feature.properties) {
-//             layer.bindPopup(Object.keys(feature.properties).map(function (k) {
-//                 return k + ": " + feature.properties[k];
-//             }).join("<br />"), {
-//                 maxHeight: 200
-//             });
-//         }
-//     }
-// });
-
 async function loadAndAddLayer(layer: RSPBLayer) {
     if (!layer.layer) {
         if (!layer.data) {
             layer.data = fetch(layer.url).then(r => r.json());
+            if(layer.process) {
+                layer.data = layer.process(layer.data);
+            }
         }
         const data = await layer.data;
 
